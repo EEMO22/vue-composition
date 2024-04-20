d4c7286b13ee0a3d69df441f93fe7f175082a253
강호진 <battlemech@naver.com>
2024-04-20 23:14:46 +0900

    Composition API 개념과 장점

--- vue-composition/src/App.vue
+++ vue-composition/src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>{{ message }}</p>
+    <button @click="changeMessage">change</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import { ref } from 'vue';
+import { useMessage } from './hooks/useMessage'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    setup() {
+
+      //  타입 추론이 잘 된다 (Vue3는 코어 라이브러리가 싹 다 TypeScript)
+      //  ** Composition API 의 첫 번째 강력한 장점! **
+
+      //  구조 분해 문법
+      const { message, changeMessage } = useMessage();
+
+      //  아래의 로직은 재사용(또는 다른 곳에서 사용)을 위해 useMessage.js로 이전 후 가져와 사용
+      //  ** Composition API 의 두 번째 강력한 장점! **
+
+      // //  data
+      // const message = ref('hello');
+
+      // //  methods
+      // function changeMessage() {
+      //   message.value = 'hi';
+      // }
+
+      //  return해야 setup 바깥에서 사용 가능
+      return { message, changeMessage }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+
 </style>
